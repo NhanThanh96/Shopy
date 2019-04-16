256aa026ba758b79d76631abb2c1ac9656e404c9
NhanThanh96 <thanhnhan030796@gmail.com>
2019-04-16 18:48:56 +0700

    edit header

---
+++ source/js/_custom.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  new PerfectScrollbar(".js-activities-scrollbar", {
+    wheelPropagation: true
+  });
+  new PerfectScrollbar(".js-messages-scrollbar", {
+    wheelPropagation: true
+  });
+});
